5d22d9456bffee5f3c7cacfdddd43d64735caeff
ivan <ivana50272@students.laalliance.org>
2018-06-13 17:16:46 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require("express")
+var logger = require("morgan")
+var bodyParser = require("body-parser")
+
+var server = express()
+
+server.set('view engine', 'ejs')
+server.use(express.static('views'))
+server.set('views', __dirname+ '/views')
+
+server.get('/', function(request, response){
+    response.render('home.ejs')
+})
+server.get('/about-me', function(request, response){
+    response.send('<h1>About me</h1>')
+})
+
+
+var port = 8080
+
+server.listen(port, () =>{
+    console.log(' Server on port ' + port)
+})
